8f4cd33f2db2d58a834411cb068f55273e6ecfe5
Marcus <mvmadu@gmail.com>
2014-09-17 12:36:44 -0300

    Events criado

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903145108) do
+ActiveRecord::Schema.define(version: 20140917152806) do
+
+  create_table "events", force: true do |t|
+    t.string   "title"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
