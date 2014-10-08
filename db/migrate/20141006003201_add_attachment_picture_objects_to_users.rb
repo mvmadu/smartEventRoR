fe47caf51aa72bf96e48ab1e91ee1aaf2f3a067b
Marcus Vinnicius R. Madureira <mvmadu@gmail.com>
2014-10-07 21:26:31 -0300

    Semanas atualizado

--- db/migrate/20141006003201_add_attachment_picture_objects_to_users.rb
+++ db/migrate/20141006003201_add_attachment_picture_objects_to_users.rb
@@ -1,7 +1,8 @@
 class AddAttachmentPictureObjectsToUsers < ActiveRecord::Migration
   def self.up
-    change_table :events do |t|
-      t.attachment :picture_objects
+    #change_table :events do |t|
+      #t.attachment :picture_objects
+      add_attachment :events, :picture_objects
     end
   end
 
