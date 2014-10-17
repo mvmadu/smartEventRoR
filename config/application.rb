777ca9789c40fb9397accf222b4f9b1ab8b0841e
Bernardormg <bernardormg@hotmail.com>
2014-10-17 16:34:44 -0300

    Colocando no heroku

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module SmartEventRoR
     #to enable rails' asset pipleline
     config.assets.enabled = true 
     config.serve_static_assets = true
+    config.assets.initialize_on_precompile = false
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
